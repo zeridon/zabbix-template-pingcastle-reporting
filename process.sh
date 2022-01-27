67d7df0551ec93403ed1f48da23ac66b4ac131e4
Vladimir Vitkov <vvitkov@gmail.com>
2022-01-27 16:16:47 +0200

    add shellcheck

--- process.sh
+++ process.sh
@@ -14,28 +14,28 @@ _inputfile=$1
 _processstamp=$(date '+%F_%R')
 
 function cleanup() {
-	rm -rf ${_workdir}
+	rm -rf "${_workdir}"
 }
 
-trap cleanup ERR EXIT SIGINT SIGHUP SIGQUIT SIGKILL SIGTERM
+trap cleanup ERR EXIT SIGINT SIGHUP SIGQUIT SIGTERM
 
 # Get some nice stuff around the domain
 # descriptive info
-_domain_name=$(xmllint --xpath '/HealthcheckData/DomainFQDN/text()' ${_inputfile})
-_domain_sid=$(xmllint --xpath '/HealthcheckData/DomainSid/text()' ${_inputfile})
+_domain_name=$(xmllint --xpath '/HealthcheckData/DomainFQDN/text()' "${_inputfile}")
+_domain_sid=$(xmllint --xpath '/HealthcheckData/DomainSid/text()' "${_inputfile}")
 
 # timestamp when the report was generated (for zabbix trapper)
-_t=$(xmllint --xpath '/HealthcheckData/GenerationDate/text()'${_inputfile})
+_t=$(xmllint --xpath '/HealthcheckData/GenerationDate/text()' "${_inputfile}")
 _unixtimestamp=$(date -d "${_t}" +%s)
 
 for _key in EngineVersion GlobalScore StaleObjectsScore PrivilegiedGroupScore TrustScore AnomalyScore ; do
-	echo ${_domain_sid} pingcastle.${_key} ${_unixtimestamp} $(xmllint \
+	echo "${_domain_sid}" pingcastle."${_key}" "${_unixtimestamp}" "$(xmllint \
 		--xpath "/HealthcheckData/${_key}/text()" \
-		${_inputfile}) >> ${_workdir}/zabbix_data
+		"${_inputfile}")" >> "${_workdir}"/zabbix_data
 done
 
-echo ${_domain_sid} pingcastle.PrivilegiedGroups.DomainAdministrators ${_unixtimestamp} $(xmllint \
+echo "${_domain_sid}" pingcastle.PrivilegiedGroups.DomainAdministrators "${_unixtimestamp}" "$(xmllint \
 	--xpath '/HealthcheckData/PrivilegedGroups/HealthCheckGroupData/GroupName[contains(text(),"Domain Administrators")]/following-sibling::NumberOfMemberEnabled/text()' \
-	${_inputfile}) >> ${_workdir}/zabbix_data
+	"${_inputfile}")" >> "${_workdir}"/zabbix_data
 
-zabbix_sender -z ${_ZABBIX_SERVER} -T -i ${_workdir}/zabbix_data
+zabbix_sender -z "${_ZABBIX_SERVER}" -T -i "${_workdir}"/zabbix_data
